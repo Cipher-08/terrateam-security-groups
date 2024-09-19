5f466bda6fab87bc926cd75ae56e5d0c525ace72
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-09-19 22:29:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "terrateam_bucket_0001" {
+resource "aws_s3_bucket" "terrateam_bucket_03" {
   bucket = "s03-tf-bucket"
   acl    = "private"
 }
