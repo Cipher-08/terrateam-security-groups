1c08103cd340c53780944565033139c883a99da5
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-09-19 22:15:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "terrateam-state-bucket"
-    key            = "terraform/state"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
-
-resource "aws_s3_bucket" "terrateam_bucket_03" {
+resource "aws_s3_bucket" "terrateam_bucket_0001" {
   bucket = "s03-tf-bucket"
   acl    = "private"
 }
