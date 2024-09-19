6856d8ed069074d0774da4cd0cd10553cf9cd66f
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-09-19 13:55:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terrateam-state-bucket"
+    key            = "terraform/state"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+  }
+}
+
+resource "aws_s3_bucket" "terrateam_bucket_03" {
+  bucket = "s03-tf-bucket"
+  acl    = "private"
+}
