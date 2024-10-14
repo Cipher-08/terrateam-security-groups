6cad48ba887fa54c8ae234df97fda94ed587e434
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-10-14 15:51:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+}
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
 }
 
+variable "aws_secret_access_key" {
+  type      = string
+  sensitive = true
+}
 resource "aws_s3_bucket" "terrateam_bucket_03" {
   bucket = "s03-tf-bucket"
   acl    = "private"
